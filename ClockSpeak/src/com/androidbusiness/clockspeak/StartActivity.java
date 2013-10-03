9a5bf8486c8eb5086c36caf8aa66329d7197b073
Diego <daer13@gmail.com>
2013-10-03 22:59:21 +0200

    ADD: Listeners de los 2 botones

--- ClockSpeak/src/com/androidbusiness/clockspeak/StartActivity.java
+++ ClockSpeak/src/com/androidbusiness/clockspeak/StartActivity.java
@@ -1,23 +1,34 @@
 package com.androidbusiness.clockspeak;
 
+
+import com.androidbusiness.clockspeak.listeners.CustomOnClickListener;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class StartActivity extends Activity {
 
+	private static final String TAG = StartActivity.class.getSimpleName();
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_start);
 		
 		Spinner sp = (Spinner) findViewById(R.id.list_minutes);
-		ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.list_minutes, android.R.layout.simple_spinner_item);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.list_minutes, android.R.layout.simple_spinner_item);
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		sp.setAdapter(adapter);
 		
+		Button btn_start = (Button)findViewById(R.id.btn_start);
+		btn_start.setOnClickListener(new CustomOnClickListener());
+		Button btn_close = (Button)findViewById(R.id.btn_stop);
+		btn_close.setOnClickListener(new CustomOnClickListener());
 	}
 
 	@Override
@@ -27,4 +38,5 @@ public class StartActivity extends Activity {
 		return true;
 	}
 
+	
 }
---
+++ ClockSpeak/src/com/androidbusiness/clockspeak/listeners/CustomOnClickListener.java
@@ -0,0 +1,34 @@
+package com.androidbusiness.clockspeak.listeners;
+
+import com.androidbusiness.clockspeak.R;
+import com.androidbusiness.clockspeak.R.id;
+
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class CustomOnClickListener implements OnClickListener {
+	private static final String TAG = CustomOnClickListener.class.getSimpleName();
+
+	@Override
+	public void onClick(View boton) {
+		Log.d(TAG, "click en "+boton.getId());
+		switch (boton.getId()) {
+		case R.id.btn_start:
+			Log.d(TAG, "click en Start");
+			
+			
+			break;
+		case R.id.btn_stop:
+			Log.d(TAG, "click en Stop");
+			
+			
+			break;
+
+		default:
+			break;
+		}
+		
+	}
+
+}
